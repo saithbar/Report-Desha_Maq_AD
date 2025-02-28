c32e5194a9a9bab169dbd2f32ca7355bc060a1c5
Saith Barreto <33233515+saithbar@users.noreply.github.com>
2025-02-28 11:43:56 +0100

    Script for Report of machine with more 180 days not report to AD

---
+++ reporte_maq_desha.ps1
@@ -0,0 +1,19 @@
+﻿#*******Begin Comment**************
+.SYNOPSIS
+#	This script performs the  uninstallation of an application(s).
+#  Script for Report of machine with more 180 days not report to AD
+# version V1
+# Autor: Saith Barreto
+# Fecha: Septiembre 2024
+#*******End Comment**************
+import-module activedirectory  
+#$domain = "ags-spain.local"
+$searchOU = "ou=WIP,DC=ags-spain,DC=local"  
+$DaysInactive = 180  
+$time = (Get-Date).Adddays( - ($DaysInactive)) 
+  
+# pilla el lastlongon y lo contrasta 
+Get-ADComputer -SearchBase $searchOU -Filter { LastLogonTimeStamp -lt $time } -Properties LastLogonTimeStamp | 
+  
+# mete los nombres de las maquinas en un csv.
+select-object Name, @{Name = "Stamp"; Expression = { [DateTime]::FromFileTime($_.lastLogonTimestamp) } } | export-csv C:\temp\Maquinas_inactivas.csv
